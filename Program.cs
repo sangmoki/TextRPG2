0688a8a7eb7dbde13a5f316343595ca57a9d7a62
sangmoki <yoochang4176@naver.com>
2024-07-10 11:30:13 +0900

    Feat: "플레이어 공격 기능 및 hp 감소 기능 구현"

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace TextRPG2
         static void Main(string[] args)
         {
             // player 객체 생성 및 초기화
-            Archer archer = new Archer();
+            Player player = new Archer();
+            Monster monster = new Orc();
+
+            // 플레이어 공격력 불러오기
+            int damage = player.GetAttack();
+            // 플레이어의 공격력만큼 몬스터가 공격받음
+            monster.OnDamaged(damage);
         }
     }
 }
